c06b3904eae730d20dcb835bf98b0a741900058b
lisaw97 <lisawen0614@gmail.com>
2020-04-07 17:46:00 -0700

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,17 @@ import React from "react"
 const Header = () => (
   <header>
     <div className="container">
-      <div className="logo">
-        <Link to="/">ODUNSI</Link>
-      </div>
-      <div className="navigation">
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/work">Work</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
+      <div className="inner-header">
+        <div className="logo">
+          <Link to="/">ODUNSI</Link>
+        </div>
+        <div className="navigation">
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/work">Work</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </div>
       </div>
     </div>
   </header>
