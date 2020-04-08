253e9df913713247aa4ee9fd44b613d707501777
lisaw97 <lisawen0614@gmail.com>
2020-04-07 18:51:29 -0700

    Add and style images in banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,13 +1,47 @@
 import React from 'react'
-// import { useStaticQuery, graphql } from "gatsby"
-// import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
 
 const Banner = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      poppinShades: file(relativePath: { eq: "poppin-shades.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      diamonds: file(relativePath: { eq: "diamonds.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      orange: file(relativePath: { eq: "sexy-orange.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <div className="banner">
       <div className="container">
         <div className="row">
+          <div className="side-image left">
+            <Img fluid={data.diamonds.childImageSharp.fluid} />
+          </div>
           <div className="main-text">Lisa Wen</div>
+          <div className="main-image">
+            <Img fluid={data.poppinShades.childImageSharp.fluid} />
+          </div>
+          <div className="side-image right">
+            <Img fluid={data.orange.childImageSharp.fluid} />
+          </div>
         </div>
       </div>
     </div>
